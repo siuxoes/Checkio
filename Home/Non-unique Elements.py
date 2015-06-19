658579e3bc031c03b3cbf6c6ea98fffc23bde9bc
Sebastian Leonte <sebastianleonteandroid@gmail.com>
2015-06-20 01:45:22 +0200

    Home added

---
+++ Home/Non-unique Elements.py
@@ -0,0 +1,17 @@
+__author__ = 'Siuxoes'
+
+def checkio(data):
+    otherArray = []
+    for x in data:
+        if data.count(x) > 1:
+            otherArray.append(x)
+    return otherArray
+
+
+if __name__ == "__main__":
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert isinstance(checkio([1]), list), "The result must be a list"
+    assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3], "1st example"
+    assert checkio([1, 2, 3, 4, 5]) == [], "2nd example"
+    assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5], "3rd example"
+    assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9], "4th example"
