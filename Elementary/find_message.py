06a3aee47558aaf0f3b4b2ec367e799aa22f2dca
Sebastian Leonte <sebastianleonteandroid@gmail.com>
2015-06-20 00:34:28 +0200

    Added find_message file

---
+++ Elementary/find_message.py
@@ -0,0 +1,24 @@
+__author__ = 'Siuxoes'
+
+def find_message(text):
+    result = [l for l in text if l.isupper()]
+    return "".join(result)
+
+#Another way to do it
+def find_message2(text):
+    result = ""
+    for x in text:
+        if x.isupper():
+            result += x
+    return result
+
+text = "How are you? Eh, ok. Low or Lower? Ohhh."
+
+print(find_message(text)) # should print HELLO
+print(find_message2(text)) # should print HELLO
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert find_message("How are you? Eh, ok. Low or Lower? Ohhh.") == "HELLO", "hello"
+    assert find_message("hello world!") == "", "Nothing"
+    assert find_message("HELLO WORLD!!!") == "HELLOWORLD", "Capitals"
